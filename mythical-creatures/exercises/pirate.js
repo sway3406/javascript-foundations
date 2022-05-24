b0026a981ff00277f659a833e13897155ef31c58
Shane Way <shane.c.way2@gmail.com>
2022-05-24 08:36:33 -0600

    This commit adds Pirate unit-test

---
+++ mythical-creatures/exercises/pirate.js
@@ -0,0 +1,14 @@
+class Pirate {
+  constructor(name, job, cursed) {
+    this.name = name;
+    this.job = job || 'scallywag';
+    this.cursed = false;
+  }
+
+robShip() {
+    return `YAARRR!`;
+  }
+}
+
+
+module.exports = Pirate;
