86d36e2212f8a0555839e4e3a21988ecc5b851b9
Shane Way <shane.c.way2@gmail.com>
2022-05-24 17:17:17 -0600

    This commit completes hobbit implementation test

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,22 +1,29 @@
 class Hobbit {
-  constructor({name}, age) {
-    this.name = name;
+  constructor(object) {
+    this.name = object.name;
     this.age = 0;
     this.adult = false;
+    this.old = false;
+    this.hasRing = false;
+}
 
-  }
   celebrateBirthday(age) {
     this.age++
-  }
-  timeTravel(age) {
-  // for(var i = 0; i >= this.age; i++)
-    if (this.age <= 32) {
-      this.adult = false;
-    } else {
+    if (this.age > 32) {
       this.adult = true;
-      }
+    if (this.age > 100)
+      this.old = true;
     }
   }
+  getRing() {
+    if (this.name === 'Frodo') {
+      this.hasRing = true;
+      return 'Here is the ring!';
+    } else {
+      return `You can\'t have it!`;
+    }
+  }
+}
 
 
 
