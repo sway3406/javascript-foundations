38369c557f492c0af6422551adee112f5ec50c64
Shane Way <shane.c.way2@gmail.com>
2022-05-24 19:02:15 -0600

    This commit completes pirate.js

--- mythical-creatures/exercises/pirate.js
+++ mythical-creatures/exercises/pirate.js
@@ -1,12 +1,28 @@
 class Pirate {
-  constructor(name, job, cursed) {
+  constructor(name, job, cursed, booty) {
     this.name = name;
     this.job = job || 'scallywag';
     this.cursed = false;
+    this.booty = 0;
 }
 
   robShip() {
-    return `YAARRR!`;
+  if (this.booty >= 500) {
+      this.cursed = true;
+      return `ARG! I\'ve been cursed!`
+    } else {
+      this.booty+= 100
+      return `YAARRR!`;
+    }
+  }
+  liftCurse() {
+  if(this.booty < 500) {
+    return 'You don\'t need to lift a curse!';
+  } else {
+    this.booty = 200;
+    this.cursed = false;
+    return 'Your curse has been lifted!';
+    }
   }
 }
 
