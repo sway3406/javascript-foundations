1d5bb4a8b8217f415a14240f4386e5e8104c3e4b
Shane Way <shane.c.way2@gmail.com>
2022-05-24 21:02:48 -0600

    This commit completes magician.js

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -3,16 +3,25 @@ class Magician {
     this.name = `The Great ${object.name}`;
     this.assistant = object.assistant;
     this.favoriteAccessory = object.clothing || 'top hat';
+    this.confidencePercentage = 10;
   }
 
   performIncantation(incantation) {
    return `${incantation.toUpperCase()}!`;
   }
   performTrick(incantation) {
+    this.confidencePercentage+=10
     if(this.favoriteAccessory === 'top hat') {
     return `PULL RABBIT FROM TOP HAT`;
       } else {
       return `PULL DOVE FROM SLEEVE`;
+    }
+  }
+  performShowStopper() {
+    if(this.confidencePercentage >= 100 && this.assistant === true) {
+      return 'WOW! The magician totally just sawed that person in half!';
+    } else {
+      return 'Oh no, this trick is not ready!';
     }
   }
 }
