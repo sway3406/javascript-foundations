cc44db19299d997705150ea201f4bb85c0fb4e67
Shane Way <shane.c.way2@gmail.com>
2022-05-26 10:38:13 -0600

    This commit updates code in Skier.js

--- ski-lift/src/Skier.js
+++ ski-lift/src/Skier.js
@@ -1,5 +1,18 @@
 class Skier {
-
+  constructor(name, hasLiftTicket, skillLevel, nextSlope) {
+    this.name = name;
+    this.hasLiftTicket = true;
+    this.skillLevel = 1;
+    this.nextSlope = 'green circle';
+  }
+  takeLesson() {
+    this.skillLevel+=1
+  }
+  pickSlope() {
+    if(this.skillLevel > 2) {
+      this.nextSlope = 'blue square';
+    }
+  }
 }
 
 module.exports = Skier;
