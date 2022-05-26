081e765184f3947b9d4b3c4919991a357e4a4765
Shane Way <shane.c.way2@gmail.com>
2022-05-26 15:31:37 -0600

    This commit completes medusa.js

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -7,6 +7,21 @@ class Medusa {
     this.statues = [];
 
   }
+  gazeAtVictim(person) {
+    var newStatue = new Statue(person.name);
+      if (this.statues.length <= 2) {
+        this.statues.push(newStatue);
+    } else if (this.statues.length >= 3) {
+      var revivedPerson = new Person(this.statues[0].name);
+      this.statues.shift();
+      this.statues.push(newStatue);
+      revivedPerson.mood = 'relieved';
+      return revivedPerson;
+    }
+  }
 }
+
+
+
 
 module.exports = Medusa;
