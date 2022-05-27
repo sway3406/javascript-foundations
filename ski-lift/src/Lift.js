d6f5ade06e53f7495ab092b781f2547fcc4e1725
Shane Way <shane.c.way2@gmail.com>
2022-05-26 20:08:51 -0600

    This commit adds lift.js code

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -1,7 +1,23 @@
 var Skier = require('./Skier');
 
 class Lift {
-  
-}
+  constructor(limit) {
+    this.inService = true;
+    this.limit = limit;
+    this.skiers = [];
+    this.safetyBar = 'up';
+  }
+  admitSkier(name, liftTicket) {
+    var newSkier = new Skier(name, liftTicket);
+    if (newSkier.hasLiftTicket === false) {
+      return `Sorry, ${newSkier.name}. You need a lift ticket!`;
+    } else if (this.skiers.length < this.limit) {
+        this.skiers.push(newSkier);
+        console.log(this.skiers);
+        } else {
+        return `Sorry, ${newSkier.name}. Please wait for the next lift!`
+      }
+    }
+  }
 
 module.exports = Lift;
