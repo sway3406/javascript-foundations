3b8a078a2faad5b7fe93a5cf1ef1eb77dcaab8a1
Shane Way <shane.c.way2@gmail.com>
2022-05-26 20:06:03 -0600

    This commit adds material.js code

--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,5 +1,22 @@
 class Material {
-
+  constructor(name, price, amount, units){
+    this.name = name;
+    this.price = price;
+    this.amount = amount;
+    this.units = units;
+  }
+  useMaterial(amountUsed) {
+    if (amountUsed > this.amount) {
+      return `You don\'t have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`
+    } else {
+      this.amount-=amountUsed
+      return `You now have ${this.amount} ${this.units} of ${this.name} left.`;
+    }
+  }
+  calculateMaterialCost() {
+  var totalCost = this.amount * this.price;
+  return totalCost;
+  }
 }
 
 module.exports = Material;
