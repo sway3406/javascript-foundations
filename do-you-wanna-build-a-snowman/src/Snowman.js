2c2d0c4d3d682d948b24d4f7472f5574c4a95d41
Shane Way <shane.c.way2@gmail.com>
2022-05-26 23:31:19 -0600

    This commit updates Snowman.js code

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,18 @@
 class Snowman {
-
+  constructor(snowPerson) {
+    this.carrots = snowPerson.carrots;
+    this.coal = snowPerson.coal;
+    this.buttons = snowPerson.buttons;
+    this.snowballs = snowPerson.snowballs;
+    this.magicHat = false;
+  }
+  canWearMagicHat() {
+    if(this.coal < 2 || this.buttons < 5 || this.carrots < 1 || this.snowballs < 2) {
+      this.magicHat = false
+      } else {
+      this.magicHat = true;
+    }
+  }
 }
 
 module.exports = Snowman;
