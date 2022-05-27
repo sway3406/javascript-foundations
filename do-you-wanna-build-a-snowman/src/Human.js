50a39ddd341ee4469afaeda094c7f02ef01cf628
Shane Way <shane.c.way2@gmail.com>
2022-05-26 23:30:17 -0600

    This commit updates code for Human.js

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,19 @@
 var Snowman = require('./Snowman');
 
 class Human {
-
+  constructor(name) {
+    this.name = name;
+    this.wantsToBuildASnowman = true;
+    this.materials = {
+      snowballs: 0,
+      coal: 0,
+      buttons: 0,
+      carrots: 0
+    };
+  }
+  gatherMaterials(materialsGathered, material) {
+    this.materials[materialsGathered] = material;
+  }
 }
 
 module.exports = Human;
