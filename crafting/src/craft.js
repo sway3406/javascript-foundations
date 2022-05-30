931f0c986e7e204ff64d5888108be2fdecceefc8
Shane Way <shane.c.way2@gmail.com>
2022-05-29 21:28:14 -0600

    This commit adds passes all tests except for final test

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,15 @@
 class Craft {
-
+  constructor(craft) {
+    this.name = craft.type;
+    this.materials = craft.materials;
+    this.completed = false;
+  }
+  completeCraft() {
+    this.completed = true;
+    return `All done! It looks great!`;
+  }
+  calculateProjectTotal() {
+ 
+  }
 }
-
 module.exports = Craft;
