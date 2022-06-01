0fde4e42d6c21153ff674403eb19d5dd955012eb
Shane Way <shane.c.way2@gmail.com>
2022-05-31 22:04:02 -0600

    This commit completes all implementation code for ski-lift

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -13,11 +13,22 @@ class Lift {
       return `Sorry, ${newSkier.name}. You need a lift ticket!`;
     } else if (this.skiers.length < this.limit) {
         this.skiers.push(newSkier);
-        console.log(this.skiers);
         } else {
         return `Sorry, ${newSkier.name}. Please wait for the next lift!`
       }
     }
+  startLift() {
+    var skiersNeeded = this.limit - this.skiers.length;
+    if (this.skiers.length < this.limit && skiersNeeded === 1) {
+        this.safetyBar = `up`;
+        return `We still need ${skiersNeeded} more skier!`;
+    } else if (this.skiers.length < this.limit) {
+        return `We still need ${skiersNeeded} more skiers!`;
+    } else {
+     return this.safetyBar = `down`;
   }
+ }
+}
+
 
 module.exports = Lift;
