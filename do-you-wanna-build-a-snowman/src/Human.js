0523783c7203776b2b5ac86afc8066193e3558b8
Shane Way <shane.c.way2@gmail.com>
2022-06-02 06:44:14 -0600

    This commit completes implementation test file

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -12,8 +12,32 @@ class Human {
     };
   }
   gatherMaterials(materialsGathered, material) {
-    this.materials[materialsGathered] = material;
+    this.materials[materialsGathered] += material;
+  }
+  buildASnowman() {
+    var snowman = new Snowman(this.materials);
+    return snowman;
+}
+
+// This test works w/o modifying the test suite
+placeMagicHat() {
+  if (this.materials.coal < 2 || this.materials.buttons < 5 || this.materials.carrots < 1 || this.materials.snowballs < 2) {
+  return 'I guess I didn\'t build it correctly.';
+  } else {
+    return 'Woah, this snowman is coming to life!';
+    }
   }
 }
+
+
+//This fxn requires us to modify the test by adding snowman into each of the placeMagicHat arguments.
+//   placeMagicHat(snowman) {
+//     if (snowman.magicHat === true) {
+//     return 'Woah, this snowman is coming to life!';
+//   } else {
+//     return 'I guess I didn\'t build it correctly.';
+//     }
+//   }
+// }
 
 module.exports = Human;
