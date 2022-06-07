159570a66f0d53ac00c6bec2624cd3cb692cb0d6
Shane Way <shane.c.way2@gmail.com>
2022-06-06 22:59:53 -0600

    This commit updates the Stark Class up through test 13

--- mythical-creatures/exercises/stark.js
+++ mythical-creatures/exercises/stark.js
@@ -1,7 +1,11 @@
 const Direwolf = require('../exercises/direwolf');
 
 class Stark {
+  constructor(starkDetails) {
+    this.name = starkDetails.name;
+    this.location = starkDetails.area || 'Winterfell';
 
+  }
 }
 
 
