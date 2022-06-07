be56ada68d75b153ed7528abec137344e7ea99c7
Shane Way <shane.c.way2@gmail.com>
2022-06-06 22:58:04 -0600

    This commit passes tests first 13 tests

--- mythical-creatures/exercises/direwolf.js
+++ mythical-creatures/exercises/direwolf.js
@@ -1,7 +1,15 @@
 const Stark = require('../exercises/stark');
 
 class Direwolf {
-  
+  constructor(name, home, size) {
+    this.name = name;
+    this.home = home || 'Beyond the Wall';
+    this.size = size || 'Massive';
+    this.starksToProtect = [];
+  }
+  protect(starkDetails) {
+    this.starksToProtect.push(starkDetails);
+  }
 }
 
 
