674c6278238f3dc2db0be98ced762877745313b3
Shane Way <shane.c.way2@gmail.com>
2022-06-10 08:50:58 -0600

    This commit completes Roadrace test suite and the associated implementation code

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,26 @@
 class Roadrace {
+  constructor(raceObj) {
+    this.name = raceObj.title;
+    this.location = raceObj.city;
+    this.distance = 0;
+    this.participants = [];
 
+  }
+  setDistance(raceDistance) {
+    this.distance = raceDistance;
+    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`
+  }
+  registerParticipants(runner) {
+    this.participants.push(runner);
+  }
+  completeRace() {
+    for(var i = 0; i < this.participants.length; i++) {
+      this.participants[i].runRace(this.name, this.distance);
+    }
+  }
 }
+//The code is less DRY than line 18
+// this.participants[i].completedRaces.push(this.name);
+// this.participants[i].runSomeMiles(this.distance);
 
-module.exports = Roadrace;
+module.exports = Roadrace
