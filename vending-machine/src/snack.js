6229f2f464a6a9be5efbee1041115d52a79efefb
Shane Way <shane.c.way2@gmail.com>
2022-06-21 22:17:45 -0600

    This commit completes snack.js implementation code

--- vending-machine/src/snack.js
+++ vending-machine/src/snack.js
@@ -1,5 +1,20 @@
 class Snack {
-
+constructor(name, price) {
+    this.name = name;
+    this.price = price;
+    this.itemsInStock = 0;
+    }
+    stockItems(snackAmount) {
+        this.itemsInStock += snackAmount;
+    }
+    removeItem() {
+        if (this.itemsInStock <= 0) {
+            return `Sorry, no ${this.name} left in stock!`
+        } else {
+        this.itemsInStock--;
+        return `Item taken! There are now ${this.itemsInStock} items left.`
+        }
+    }
 }
 
 module.exports = Snack;
