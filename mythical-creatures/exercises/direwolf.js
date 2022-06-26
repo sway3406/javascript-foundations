a0269a58348e5a14ee7f480fc5c5e7e43570f31f
Shane Way <shane.c.way2@gmail.com>
2022-06-25 22:58:54 -0600

    This commit updates stark.js, direwolf.js and the test suite through -should change house once protected--

--- mythical-creatures/exercises/direwolf.js
+++ mythical-creatures/exercises/direwolf.js
@@ -1,7 +1,21 @@
 const Stark = require('../exercises/stark');
 
 class Direwolf {
-  
+    constructor(name, home, size) {
+        this.name = name;
+        this.home = home || 'Beyond the Wall';
+        this.size = size || 'Massive';
+        this.starksToProtect = [];
+
+    }
+    protect(stark) {
+        if (this.starksToProtect.length >= 2) {
+            return;
+        } else if (this.home === stark.location) {   
+            stark.safe = true; 
+            this.starksToProtect.push(stark);
+        }
+    }
 }
 
 
--- mythical-creatures/exercises/stark.js
+++ mythical-creatures/exercises/stark.js
@@ -1,7 +1,18 @@
 const Direwolf = require('../exercises/direwolf');
 
 class Stark {
-
+    constructor(starkObj) {
+        this.name = starkObj.name;
+        this.location = starkObj.area || 'Winterfell';
+        this.safe = false;
+    }
+    sayHouseWords(){
+        if (this.safe === true) {
+            return 'The North Remembers'
+        } else {
+            return 'Winter is Coming'
+        }
+    }
 }
 
 
